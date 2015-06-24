fcd76563ce9397e48f11bc75750499c4c42fa9b3
Jean-Marie Madiot <madiot@gmail.com>
2015-06-24 18:58:02 +0100

    flights between airports, instead of between cities -- type A to activate

--- web/omap.js
+++ web/omap.js
@@ -9,6 +9,9 @@ var vectorLayer = [];
 /* prevent redraw */
 var freeze = false;
 
+/* take into account airports? */
+var AIRPORTS = 0;
+
 /* enable debugging */
 var DEBUG=0;
 var PRINT_TIMES_TO_CITIES=0;
@@ -26,12 +29,14 @@ var dist = function(pos1, pos2) {
   return Math.abs(d);
 }
 
+/* pretty-printing of a duration */
 var nicetime = function(minutes) {
   var h = Math.floor(minutes/60);
   var m = Math.floor(minutes%60);
   var z = (m<10)?'0':'';
   return h + ":" + z + m;
 }
+
 var closest_cities = new Array();
 
 /* kms per minute, used to compute travel times without shorcuts */
@@ -69,6 +74,7 @@ redraw = function(e) {
   for(i=0; i<layers.length; i++){
     vectorLayer[i].getSource().clear();
   }
+  
   // computing time to reach each city from earthmouse
   time_by_car = new Array();
   timetoreach = new Array();
@@ -87,16 +93,39 @@ redraw = function(e) {
       }
     }
   }
-
-  /* display closest cities */
-  var closest = timetoreach.map(function(x,i){return [x,i]}).sort(function(x,y){return x[0]-y[0]});
+  
+  // computing time to reach each AIRPORT from earthmouse
+  air_timetoreach = new Array();
+  air_via = new Array();
+  if(AIRPORTS){
+    time_by_car = new Array();
+    for(i=0;i<flight.length;i++){
+      air_timetoreach[i] = time_by_car[i] = dist(earthmouse, airport_pos[i]) / speed;
+      air_via[i] = "";
+    }
+    for(i=0;i<flight.length;i++){
+      for(j=0;j<flight.length;j++){
+        newtime = time_by_car[i] + flight[i][j];
+        if(newtime < air_timetoreach[j]) {
+          air_timetoreach[j] = newtime;
+          air_via[j] = " (via " + airport_name[i] + ")";
+        }
+      }
+    }
+  }
+  
+  /* display closest cities/airports */
+  var closest = [];
+  closest = closest.concat(    timetoreach.map(function(time,i){return [time,       names[i],    via[i]]}));
+  closest = closest.concat(air_timetoreach.map(function(time,i){return [time,airport_name[i],air_via[i]]}));
+  closest = closest.sort(function(x,y){return x[0]-y[0]});
   closest_cities.map(function(span, i){
     if(closest[i][0]<24*60)
-      span.innerHTML = nicetime(closest[i][0]) + " to " + names[closest[i][1]] + via[closest[i][1]] + " <br/>\n"
+      span.innerHTML = nicetime(closest[i][0]) + " to " + closest[i][1] + closest[i][2] + " <br/>\n"
     else
       span.innerHTML = '';
   });
-
+  
   // draw a circle of center p and radius km on layer l
   circle = function(p, km) {
     // project the circle w.r.t web-mercator
@@ -122,6 +151,14 @@ redraw = function(e) {
       }
     }
 
+    /* with flights */
+    for(i=0; i<flight.length; i++){
+      remaining_distance = (timeavailable - air_timetoreach[i]) * speed;
+      if(remaining_distance > 0) {
+        layerCircles.appendPolygon(circle(airport_pos[i], remaining_distance));
+      }
+    }
+
     // put the polygons on the layer
     vectorLayer[layer].getSource().addFeature(new ol.Feature(layerCircles));
 
@@ -139,11 +176,10 @@ var locationsStyle = new ol.style.Style({
 /* set up overlay, map and listeners */
 init = function() {
   /* swaping positions. to be removed later */
-  for(i = 0; i < nbcities; i++){
-    var temp = pos[i][0];
-    pos[i][0] = pos[i][1];
-    pos[i][1] = temp;
-  }
+  for(i = 0; i < pos.length; i++)
+    pos[i] = [pos[i][1], pos[i][0]];
+  for(i = 0; i < airport_pos.length; i++)
+    airport_pos[i] = [airport_pos[i][1], airport_pos[i][0]];
 
   /* one layer per color */
   for(i=0; i<layers.length; i++){
@@ -195,6 +231,12 @@ init = function() {
     var newpoint = new ol.geom.Point(pos[i]); // put p[i] here
     locationsLayer.getSource().addFeature(new ol.Feature(newpoint.transform('EPSG:4326', 'EPSG:3857'))); 
   }
+  // positions of airports
+  // if(AIRPORTS) <--- cannot do that, this is a dynamic value
+  for(i=0; i<airport_pos.length; i++){
+    var newpoint = new ol.geom.Point(airport_pos[i]);
+    locationsLayer.getSource().addFeature(new ol.Feature(newpoint.transform('EPSG:4326', 'EPSG:3857'))); 
+  }
 
   /* freeze the map on click */
   map.on('singleclick', function(){freeze = !freeze;});
@@ -204,6 +246,12 @@ init = function() {
   } else {
     map.on('pointermove', redraw);
   }
+  
+  window.addEventListener('keydown', function(e) {
+    switch (String.fromCharCode(e.keyCode).toLowerCase()) {
+      case "a": AIRPORTS ^= 1;
+    }
+  });
 };
 
 window.addEventListener('load', function(){window.setTimeout(init, 500);});
